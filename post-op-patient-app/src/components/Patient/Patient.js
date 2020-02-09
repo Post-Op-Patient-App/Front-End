75fdef43508f043e61368f933e6edbcd65ef5b57
Erin Davis <davisaerin@gmail.com>
2020-02-09 01:57:00 -0600

    finished geting the colums and rows happen

--- post-op-patient-app/src/components/Patient/Patient.js
+++ post-op-patient-app/src/components/Patient/Patient.js
@@ -9,7 +9,10 @@ import {
   Dropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem
+  DropdownItem,
+  FormGroup,
+  Label,
+  Input
 } from 'reactstrap';
 
 
@@ -75,7 +78,7 @@ function Patient() {
           <CardText>Oxygen Saturation</CardText>
           <Dropdown isOpen={dropdownOpen} toggle={toggle}>
       <DropdownToggle caret>
-        Select Temp Range
+        Select Saturation Range
         </DropdownToggle>
       <DropdownMenu>
         <DropdownItem header>Percentage</DropdownItem>
@@ -85,6 +88,125 @@ function Patient() {
         <DropdownItem>{`>98.6°`}</DropdownItem>
         <DropdownItem>{`95%-97.7%°`}</DropdownItem>
         <DropdownItem>{`<95%`}</DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+        </Card>
+        </Col>
+    </Row>
+    <Row>
+    {/*This is the second row*/}
+      <Col>
+        <Card>
+          <CardTitle>L-BP</CardTitle>
+          <CardText>Prev Blood Pressure</CardText>
+          <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+      <DropdownToggle caret>
+        Select BP Range
+        </DropdownToggle>
+      <DropdownMenu>
+        <DropdownItem>{`> 130/90`}</DropdownItem>
+        <DropdownItem>{`<= 130/90 and >= 90/70`}</DropdownItem>
+        <DropdownItem>{`< 90/70`}</DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+        </Card>
+        </Col>
+        <Col>
+        <Card>
+          <CardTitle>SURF-STBL</CardTitle>
+          <CardText>Stability of Surface Temp</CardText>
+          <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+      <DropdownToggle caret>
+        Select
+        </DropdownToggle>
+      <DropdownMenu>
+        <DropdownItem>{`stable`}</DropdownItem>
+        <DropdownItem>{`mod-stable`}</DropdownItem>
+        <DropdownItem>{`unstable`}</DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+        </Card>
+        </Col>
+        <Col>
+        <Card>
+          <CardTitle>CORE-STBL</CardTitle>
+          <CardText>Core Temp Stability</CardText>
+          <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+      <DropdownToggle caret>
+        Select
+        </DropdownToggle>
+      <DropdownMenu>
+      <DropdownItem>{`stable`}</DropdownItem>
+      <DropdownItem>{`mod-stable`}</DropdownItem>
+      <DropdownItem>{`unstable`}</DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+        </Card>
+        </Col>
+    </Row>
+    <Row>
+    {/*This is the third row*/}
+      <Col>
+        <Card>
+          <CardTitle>BP-STBL</CardTitle>
+          <CardText>BP Stability</CardText>
+          <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+      <DropdownToggle caret>
+        Select
+        </DropdownToggle>
+      <DropdownMenu>
+        <DropdownItem>{`stable`}</DropdownItem>
+        <DropdownItem>{`mod-stable`}</DropdownItem>
+        <DropdownItem>{`unstable`}</DropdownItem>
+      </DropdownMenu>
+    </Dropdown>
+        </Card>
+        </Col>
+        <Col>
+        <Card>
+          <CardTitle>COMFORT</CardTitle>
+          <CardText>Perceived Comfort</CardText>
+          <FormGroup>
+        <Input type="select" name="select" id="exampleSelect">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+          <option>6</option>
+          <option>7</option>
+          <option>8</option>
+          <option>9</option>
+          <option>10</option>
+          <option>11</option>
+          <option>12</option>
+          <option>13</option>
+          <option>14</option>
+          <option>15</option>
+          <option>16</option>
+          <option>17</option>
+          <option>18</option>
+          <option>19</option>
+          <option>20</option>
+        </Input>
+      </FormGroup>
+        </Card>
+        </Col>
+        <Col>
+        <Card>
+          <CardTitle>ADM-DECS</CardTitle>
+          <CardText>Discharge Decision</CardText>
+          <Dropdown isOpen={dropdownOpen} toggle={toggle}>
+      <DropdownToggle caret>
+        Select
+        </DropdownToggle>
+      <DropdownMenu>
+      <DropdownItem>{`I`}</DropdownItem>
+      <DropdownItem>{`A`}</DropdownItem>
+      <DropdownItem>{`S`}
+      {/*There needs to be a thing where if they click this, there will be
+        a confirmation thing to make sure they were meant to be sent home*/}
+      </DropdownItem>
       </DropdownMenu>
     </Dropdown>
         </Card>
