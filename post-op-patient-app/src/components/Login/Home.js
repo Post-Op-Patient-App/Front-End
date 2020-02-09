d1e138542fd206ef33fdcfb73821b76c7168fef6
Erin Davis <davisaerin@gmail.com>
2020-02-09 04:53:14 -0600

    no bugs but no functionality

--- post-op-patient-app/src/components/Login/Home.js
+++ post-op-patient-app/src/components/Login/Home.js
@@ -1,29 +1,22 @@
 import React from "react";
-import { withFormik } from "formik";
-<<<<<<< HEAD:post-op-patient-app/src/components/Home.js
-import { Button, Form, FormGroup, Input, Card, CardHeader } from 'reactstrap';
+import { Formik, withFormik } from "formik";
+import { Button, FormGroup, Form, Input, Card, CardHeader } from 'reactstrap';
 import * as Yup from "yup";
-=======
-import { Button, Form, FormGroup, Label, Input, Card, CardHeader } from 'reactstrap';
 import './login.scss'
->>>>>>> 21995ea9b7432d3f4143d7aae71c245f309f3def:post-op-patient-app/src/components/Login/Home.js
 
 function Home(){
 
 
   return (
     <div className="home">
-<<<<<<< HEAD:post-op-patient-app/src/components/Home.js
       <Card className="login">
       <CardHeader>Log In</CardHeader>
       <Form inline className="login-form">
-=======
     <Card className="card-height">
       <h1 className="login-text">Login</h1>
->>>>>>> 21995ea9b7432d3f4143d7aae71c245f309f3def:post-op-patient-app/src/components/Login/Home.js
       <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-        <label for="exampleEmail" className="mr-sm-2">Email</label>
-        <Input type="email" name="email" id="exampleEmail" placeholder="something@idk.cool" />
+        <label for="email" className="mr-sm-2">Email</label>
+        <Input type="email" name="email" id="email" placeholder="something@idk.cool" />
       </FormGroup>
       <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
         <label for="password" className="mr-sm-2">Password</label>
@@ -37,6 +30,8 @@ function Home(){
       className="login-btn">Login</Button>
       </FormGroup>
     </Card>
+      </Form>
+      </Card>
     </div>
   )
 }
