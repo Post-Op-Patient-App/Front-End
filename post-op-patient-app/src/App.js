7464d2c7dafafba4872309b16cfd0ebf104b8378
cpscott1 <thacam18@gmail.com>
2020-02-09 02:55:17 -0600

    added sign up page

--- post-op-patient-app/src/App.js
+++ post-op-patient-app/src/App.js
@@ -3,21 +3,25 @@ import {BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
 import './App.scss';
 import Patient from "./components/Patient/Patient.js";
 import Home from "./components/Login/Home.js";
+import Register from "./components/Register/Register.js";
 
 function App(props) {
   return (
     <main>
     <Router class="container">
       <div className="nav-tabs">
-        <Link className="links" to="/">Log In</Link>
+        <Link className="links" to="/login">Log In</Link>
         <Link className="links" to="/patient/">Patient Info</Link>
       </div>
       <Switch>
         <Route path={`/patient/`}>
           <Patient id={props.id}/>
         </Route>
-        <Route path="/">
+        <Route path="/login">
           <Home />
+        </Route>
+        <Route path="/register">
+          <Register />
         </Route>
       </Switch>
       </Router>
